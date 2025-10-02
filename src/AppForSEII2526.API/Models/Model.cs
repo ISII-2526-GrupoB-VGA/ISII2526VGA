3735d1afde1f9efa2f6a64da1b53e36376d10e66
VÍCTOR MANUEL VALTUEÑA ALFARO <VictorManuel.Valtuena@alu.uclm.es>
2025-10-02 17:42:36 +0200

    Clase Model hecha sin errores.

---
+++ src/AppForSEII2526.API/Models/Model.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Model
+{
+	public int Id { get; set; }
+	public string NameModel { get; set; }
+	
+	public Model() { }
+
+	public Model(int id, string NameModel)
+	{
+		Id = id; 
+		NameModel = NameModel;
+	}
+
+	public Model(string NameModel)
+	{
+		NameModel = NameModel;
+	}
+
+	public Model(int Id)
+	{
+		Id = Id;
+	}
+
+	public override bool Equals(object obj)
+	{
+		if (obj == null || GetType() != obj.GetType())
+			return false;
+
+		Model other = (Model)obj;
+		return Id == other.Id && NameModel == other.NameModel;
+	}
+
+	public override int GetHashCode()
+	{
+		return HashCode.Combine(Id, NameModel);
+	}
+}
