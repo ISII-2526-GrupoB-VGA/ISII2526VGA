a4443ba865dab6d14f6545686719c4428f9826c1
VÍCTOR MANUEL VALTUEÑA ALFARO <VictorManuel.Valtuena@alu.uclm.es>
2025-10-29 08:43:47 +0100

    Controlador de Device y DTO creado

---
+++ src/AppForSEII2526.API/DTOs/DispostitivoParaComprarDTO/DispositivoComprarDTO.cs
@@ -0,0 +1,38 @@
+﻿namespace AppForSEII2526.API.DTOs.PurchaseDTOs
+{
+    public class DispositivoComprarDTO
+    {
+        public DispositivoComprarDTO(int id, string nombre, string marca, string modelo, string color, double precio)
+        {
+            Id = id;
+            Nombre = nombre;
+            Marca = marca;
+            Modelo = modelo;   // <- Model.NameModel
+            Color = color;
+            Precio = precio;   // <- Device.priceForPurchase
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public double Precio { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is DispositivoComprarDTO dto &&
+                   Id == dto.Id &&
+                   Nombre == dto.Nombre &&
+                   Marca == dto.Marca &&
+                   Modelo == dto.Modelo &&
+                   Color == dto.Color &&
+                   Precio == dto.Precio;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Nombre, Marca, Modelo, Color, Precio);
+        }
+    }
+}
