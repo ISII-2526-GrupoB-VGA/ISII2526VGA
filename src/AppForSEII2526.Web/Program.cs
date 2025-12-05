d2580b618c6e7d635c0fab05659d50aaa6472d74
GUILLERMO MORCILLO CONCHAN <guillermo.morcillo@alu.uclm.es>
2025-12-05 03:11:44 +0100

    Compila sin errores, no se que más ver para ver que funciona

--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -42,6 +42,7 @@ string? URI2API = builder.Configuration.GetValue(typeof(string), "AppForSEII2526
 //We create the service for accessing the API from where .WEB project
 builder.Services.AddScoped<AppForSEII2526APIClient>(sp => new AppForSEII2526APIClient(URI2API, new HttpClient()));
 
+builder.Services.AddScoped<ReviewStateContainer>(); //Esto lo añadió Guillermo, creo q esta bien
 
 var app = builder.Build();
 
